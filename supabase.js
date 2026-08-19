b9aaadae5cc2dab091eddbff9fc91849617f6321
Shadow <sephiroth.mike@gmail.com>
2026-08-19 14:50:12 +0000

    updated index.js to fix url handling

--- supabase.js
+++ supabase.js
@@ -1,6 +1,13 @@
 import { createClient } from "@supabase/supabase-js";
+import ws from "ws";
 
 export const supabase = createClient(
   process.env.SUPABASE_URL,
-  process.env.SERVICE_SUPABASESERVICE_KEY
+  process.env.SERVICE_SUPABASESERVICE_KEY,
+  {
+    realtime: {
+      transport: ws
+    }
+  }
 );
+
